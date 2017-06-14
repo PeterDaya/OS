6c6392a7b57de4fd7a5f20b943b641136b63ba1f
Peter Daya <pdaya282@gmail.com>
2017-06-14 12:36:57 -0400

    About to connect to server through wget

--- wget/wget.c
+++ wget/wget.c
@@ -4,10 +4,45 @@
 #include <sys/sockets.h>
 #include <arpa/inet.h>
 
+/* HTTP port is port 80 */
+
+static void quit(char *msg) {
+	perror(msg);
+	exit(1);
+}
+
 int main(int argc, char **argv) {
-	if (argc != 2)
-		fprintf(stderr, "Usage: ./wget <URL>\n");
+	int clntSocket;
+	char *URL, *IP, *fname, *serverName, GET[1024], rec[1024];
+	struct sockaddr_in servaddr;
+	struct hostent *he;
+	unsigned short port = 80;
+
+	if (argc != 3)
+		fprintf(stderr, "Usage: ./wget <serverName> <URL>\n");
+
+	serverName = argv[1];
+	URL = argv[2];
+
+	if ((he = gethostbyname(serverName)) == NULL)
+		quit("gethostbyname() failed");
+
+	IP = inet_ntoa(*(struct in_addr *)he->h_addr);
+	fname = strchr(URL, '\0');
+	while (*fname != '/')
+		fname--;
+	fname++;
+
+	if ((clntSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		quit("socket() failed");
+
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(IP);
+	servaddr.sin_port = htons(port);
 
 
+	
+	
 	return 0;
 }
