dace18baf4d7bc1a2be8cffa9954fac81c949ffb
Peter Daya <pdaya282@gmail.com>
2017-06-14 12:53:58 -0400

    Reading for 200 status in wget.c

--- wget/wget.c
+++ wget/wget.c
@@ -13,7 +13,7 @@ static void quit(char *msg) {
 
 int main(int argc, char **argv) {
 	int clntSocket;
-	char *URL, *IP, *fname, *serverName, GET[1024], rec[1024];
+	char *URL, *IP, *fname, *serverName, *fpath, GET[1024], rec[1024];
 	struct sockaddr_in servaddr;
 	struct hostent *he;
 	unsigned short port = 80;
@@ -23,6 +23,15 @@ int main(int argc, char **argv) {
 
 	serverName = argv[1];
 	URL = argv[2];
+
+	if (strstr(URL, ".com"))
+		fpath = strstr(URL, ".com") + 4;
+	else if (strstr(URL, ".net"))
+		fpath = strstr(URL, ".net") + 4;
+	else if (strstr(URL, ".org"))
+		fpath = strstr(URL, ".org") + 4;
+	else if (strstr(URL, ".gov"))
+		fpath = strstr(URL, ".gov") + 4;
 
 	if ((he = gethostbyname(serverName)) == NULL)
 		quit("gethostbyname() failed");
@@ -40,6 +49,32 @@ int main(int argc, char **argv) {
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = inet_addr(IP);
 	servaddr.sin_port = htons(port);
+
+	if (connect(clntSock, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
+		quit("connect() failed");
+
+	snprintf(GET, sizeof(GET), "GET %s HTTP/1.0\r\nHost: %s:%d\r\n\r\n\r\n", fpath, serverName, port);
+
+	if (send(clntSock, GET, strlen(GET), 0) != strlen(GET))
+		quit("GET request failed");
+
+	FILE *html = fopen(fname, "w");
+	if (html == NULL)
+		quit("fopen() failed");
+
+	FILE *readLine = fdopen(clntSocket, "r"); /* It's easier to read data by lines */
+	if (readLine == NULL)
+		quit("fdopen() failed");
+
+	fgets(rec, sizeof(rec), readLine);
+
+	if (!strstr(rec, "200")) {
+		fprintf(stderr, "%s\n", rec);
+		fclose(readLine);
+		fclose(html);
+		close(clntSocket);
+		quit("We did not receive a 200 response");
+	}
 
 
 	
